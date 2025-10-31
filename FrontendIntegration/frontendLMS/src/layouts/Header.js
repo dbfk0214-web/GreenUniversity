6b4cf123e6fff6a6aaac018792eb6cc65e6fd0ba
김유라 <dbfk0214@gmail.com>
2025-10-31 16:25:20 +0900

    mainlayout debug 처리
    메인레이아웃 디버그 처리하였씁니다. footer 생성 가능하신 분 부탁드립니다.

--- FrontendIntegration/frontendLMS/src/layouts/Header.js
+++ FrontendIntegration/frontendLMS/src/layouts/Header.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-const Header = ({children}) => {
+export default function Header ({children}){
   return (
     <div className="w-full bg-gray-900 h-[30%] border-red-500">{children}</div>
   )
-}
+};
 
-export default Header
