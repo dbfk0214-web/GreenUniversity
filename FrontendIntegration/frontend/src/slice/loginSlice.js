a8fcd4f24f39d5dd5e82f2c6bfcabc30701408e6
JeonByeol <star1004szx@naver.com>
2025-11-04 15:34:01 +0900

    login데이터 정보 유지

--- FrontendIntegration/frontend/src/slice/loginSlice.js
+++ FrontendIntegration/frontend/src/slice/loginSlice.js
@@ -1,38 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getCookie, removeCookie, setCookie } from "../util/cookieUtil";
+import { doLogin } from "../api/userApi";
 
-const initialState = { email: "" };
-
-const loadMemberCookie = () => {
-  const raw = getCookie("user");
-  if (!raw) return null;
-  try {
-    return JSON.parse(raw); // 문자열 → 객체
-  } catch (e) {
-    // 손상된 쿠키면 제거
-    removeCookie("user");
-    return null;
-  }
+const initialState = {
+  email: "",
+  nickname: "",
+};
+const loadAccounterCookie = () => {
+  const userInfo = getCookie("user");
+  console.log("1)userInfo:", userInfo);
+  if (userInfo && userInfo.nickname)
+    userInfo.nickname = decodeURIComponent(userInfo.nickname);
+  return userInfo;
 };
 
 const loginSlice = createSlice({
   name: "loginSlice",
-  initialState: loadMemberCookie() || initialState,
+  initialState: loadAccounterCookie() || initialState, //쿠키가 없다면 초기값 사용
   reducers: {
     login: (state, action) => {
-      const p = action.payload;
-      console.log("1)확인", p);
-      const next = typeof p === "string" ? { email: p } : p || {};
-      setCookie("user", JSON.stringify(next), 1); // 객체를 저장
-      return { ...state, ...next };
+      console.log("login....");
+      const payload = action.payload;
+      setCookie("user", JSON.stringify(payload), 1);
+      return payload;
     },
-    logout: () => {
+    logout: (state, action) => {
+      console.log("logout....");
       removeCookie("user");
       return { ...initialState };
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginPostAsync.fulfilled, (state, action) => {
+        console.log("fulfilled");
+        const payload = action.payload;
+        //정상적인 로그인시에만 저장
+        if (!payload.error) setCookie("member", JSON.stringify(payload), 1); //일
+        return action.payload;
+      })
+      .addCase(loginPostAsync.pending, (state, action) => {
+        console.log("pending");
+      })
+      .addCase(loginPostAsync.rejected, (state, action) => {
+        console.log("rejected");
+      });
+  },
 });
 
+export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) =>
+  doLogin(param)
+);
 export const { login, logout } = loginSlice.actions;
-
 export default loginSlice.reducer;
