5a2f1632aa96be78b772413da9209f0e56ba4a6e
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-11 18:06:43 +0900

    데이터 추가 및 사이트맵 작업중

--- FrontendIntegration/frontend/src/components/information/SitemapComponent.js
+++ FrontendIntegration/frontend/src/components/information/SitemapComponent.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import sitemap from "../../json/sitemap.json";
 
 const SitemapComponent = () => {
+  const [test,setTest] = useState({});
+  useEffect(() => {
+    setTest(sitemap);
+  },[])
   return (
     <div>
-      SitemapComponent
+      {test ? 
+      test.aboutGreen.map((data,idx) => 
+        (
+          <div key={idx}>{data}</div>
+        )
+      ) : <></>}
     </div>
   )
 }
 
-export default SitemapComponent
+export default SitemapComponent;
