a9ac0565006799a4fcd489b314b8b05c8ff138d3
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-11 22:32:02 +0900

    사이트맵 수정
    이제 얼추 자동화 되었습니다.

--- FrontendIntegration/frontend/src/components/information/SitemapComponent.js
+++ FrontendIntegration/frontend/src/components/information/SitemapComponent.js
@@ -1,21 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import sitemap from "../../json/sitemap.json";
 
 const SitemapComponent = () => {
-  const [test,setTest] = useState({});
+  const [test,setTest] = useState(sitemap);
+  const [keysData, setKeysData] = useState([]);
   useEffect(() => {
     setTest(sitemap);
+    console.log(test);
+    setKeysData(Object.keys(sitemap));
   },[])
+
+const makeDiv = (data) => {
+  return (
+    <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
+      {data?.map((da, idx) => (
+        <div
+          key={idx}
+          className="p-2 mb-2 bg-white rounded hover:bg-gray-100 shadow-sm"
+        >
+          {da}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
   return (
     <div>
-      {test ? 
-      test.aboutGreen.map((data,idx) => 
-        (
-          <div key={idx}>{data}</div>
-        )
-      ) : <></>}
-    </div>
-  )
+      dd
+      {keysData && keysData.map(key => (
+        <div>
+        {test[key]?.items?.map((item, idx) => (
+        <div key={idx}>{item}
+         <div>{item.title}
+              {makeDiv(test[key]?.items)}
+            </div>
+            </div>
+      ))}</div>
+  
+      ))}
+      
+    </div>)
 }
 
 export default SitemapComponent;
