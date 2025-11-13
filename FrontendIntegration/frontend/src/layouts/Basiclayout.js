3b5902f55ed9cf0aa31cab3d768d90b45f3ca67e
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-13 18:23:20 +0900

    작업중
    레이아웃 사전 작업.. 추후 삭제가능

--- FrontendIntegration/frontend/src/layouts/Basiclayout.js
+++ FrontendIntegration/frontend/src/layouts/Basiclayout.js
@@ -1,59 +1,68 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import LogoLayout from "./LogoLayout";
+import Navbar from "./Navbar";
 import Logo from "../images/university.jpg";
-import Logo2 from "../images/2.svg";
-import ddi from "../images/ddi_one.svg";
-import ddi2 from "../images/ddi_two.svg";
-import ddi3 from "../images/ddi_three.svg";
-import "../style/tailwind.css";
-import { ReactComponent as Stripe } from "../images/ddi_three.svg";
-import { hover } from "@testing-library/user-event/dist/hover";
+
 const BasicLayout = ({ children }) => {
+const datas = {
+  "aboutGreen": {
+    "campusguide": "aboutgreen/campusguide",
+    "greenvision": "aboutgreen/greenvision",
+    "historyofgreen": "aboutgreen/historyofgreen",
+    "president": "aboutgreen/president",
+    "universityoverview": "aboutgreen/universityoverview",
+    "universitysymbols": "aboutgreen/universitysymbols"
+  },
+  "academicsupport": {
+    "academicinformation": "academicsupport/academicinformation",
+    "certificatesissuance": "academicsupport/certificatesissuance",
+    "formsapplications": "academicsupport/formsapplications",
+    "scholarships": "academicsupport/scholarships",
+    "tuition": "academicsupport/tuition",
+    "undergraduatecurriculum": "academicsupport/undergraduatecurriculum"
+  },
+  "admissioneducation": {
+    "admissionguide": "admissioneducation/admissionguide",
+    "colleges": "admissioneducation/colleges",
+    "graduateschool": "admissioneducation/graduateschool",
+    "nondegreeprograms": "admissioneducation/nondegreeprograms"
+  },
+  "campuslife": {
+    "administrativeofficesinstitutes": "campuslife/administrativeofficesinstitutes",
+    "events": "campuslife/events",
+    "lostfoundboard": "campuslife/lostfoundboard",
+    "notices": "campuslife/notices",
+    "serviceguide": "campuslife/serviceguide",
+    "studentactivities": "campuslife/studentactivities"
+  },
+  "extraservices": {
+    "chatbot": "extraservices/chatbot",
+    "donate": "extraservices/donate"
+  },
+  "information": {
+    "careers": "information/careers",
+    "faq": "information/faq",
+    "legalnotice": "information/legalnotice",
+    "privacypolicy": "information/privacypolicy",
+    "sitemap": "information/sitemap"
+  },
+  "account": {
+    "login": "account/login",
+    "logout": "account/logout"
+  }
+};
   return (
     <div className="h-screen flex flex-col">
       <div className="h-20 shrink-0 bg-sky-300">
         <Header />
       </div>
-      <div className="w-full h-screen grid place-items-center bg-white">
-        <div className="relative-z w-[50%] aspect-square">
-          <div className="absolute shadow-inner" />
-          <img
-            src={Logo2}
-            alt="그린로고"
-            className="relative reveal-ccw w-full h-full rounded-full object-cover block z-100"
-          />
-          <div className="pointer-events-none absolute inset-0 z-[50] overflow-visible">
-            <div className="absolute -left-[12vw] top-10 w-[140vw] h-[160px] rotate-[24deg] origin-left">
-              <img
-                src={Stripe}
-                alt=""
-                className="block w-full h-full select-none"
-              />
-            </div>
-            <div className="absolute -left-[12vw] bottom-10 w-[140vw] h-[160px] -rotate-[-30deg] origin-right">
-              <img
-                src={Stripe}
-                alt=""
-                className="block w-full h-full select-none"
-              />
-            </div>
-            <div className="absolute -left-[12vw] bottom-10 w-[140vw] h-[160px] -rotate-[50deg] origin-left">
-              <img
-                src={Stripe}
-                alt=""
-                className="block w-full h-full select-none"
-              />
-            </div>
-          </div>
-          <div className="w-full h-screen">{children}</div>
-          <div className="w-full h-screen">Content</div>
-          <div className="w-full h-screen">Content</div>
-          <div className="w-full h-screen">Content</div>
+      <LogoLayout />
+        <Navbar children={{arrayData : datas}}/>
+        <div className="w-full h-screen">{children}</div>
           <div>
             <img src={Logo} alt="캠퍼스 사진" className="object-contain" />
           </div>
-        </div>
-      </div>
       <Footer />
     </div>
   );
