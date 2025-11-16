55e87be1267505b49efd8f06b206dfadb90dd6a6
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-17 03:07:33 +0900

    관리자페이지수정 및 관련 컴포넌트
    각종 API에 설정을 부여하여, 활용을 고민하였습니다.
    AdminComponent와 AdminLayout의 잡다한 수정

--- FrontendIntegration/frontend/src/api/AttendanceApi.js
+++ FrontendIntegration/frontend/src/api/AttendanceApi.js
@@ -3,9 +3,6 @@ import { API_SERVER_HOST, typeEnum } from "./commonApi";
 import { Await } from "react-router-dom";
 import { useState } from "react";
 
-// excludeKeys정의
-const excludeKeys = ["memo"];
-
 const Attendance = async () => {
   console.log("attendance가 가동되었다.");
   const res = await axios.get(`${API_SERVER_HOST}/api/attendance/student/checkclass`);
@@ -19,29 +16,55 @@ const readAll = async () => {
 };
 
 const config = {
+  tableInfo: {
+    tableEng: "Attendance",
+    tableName: "출석",
+  },
   columns: {
     attendance: "이름",
     localDateTime: "출석일",
     status: "상태",
   },
-  func:{Attendance,readAll},
-  excludeKeys: excludeKeys,
+  funcs: { Attendance, readAll },
   formData: {},
-  type:typeEnum.default
+  type: typeEnum.default
 };
 
 
-export default {config};
+export default { config };
 // 출석 확인
 
 // export const attendanceHandler = async () => {
 //   const attendanceData = {
 //     userId: Number(userId), //input에서 받은 값
-//     date,                              
-//     status: "PRESENT",               
+//     date,
+//     status: "PRESENT",
 //     memo
 //   };
 //   const res = await axios.post(`${API_SERVER_HOST}/api/attendance`, attendanceData);
 //   console.log(res.data);
 // };
 //출석 입력
+
+
+// 더미 데이터
+/* const [dummyData, setDummyData] = useState([
+  {
+    name: "홍길동",
+    date: "2025-11-12",
+    status: "출석",
+    memo: "지각 없음",
+  },
+  {
+    name: "김철수",
+    date: "2025-11-12",
+    status: "결석",
+    memo: "사유서 제출 예정",
+  },
+  {
+    name: "이영희",
+    date: "2025-11-12",
+    status: "지각",
+    memo: "10분 늦음",
+  },
+]); */
--- FrontendIntegration/frontend/src/api/CourseOfferingApi.js
+++ FrontendIntegration/frontend/src/api/CourseOfferingApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_SERVER_HOST } from "./commonApi";
+import { API_SERVER_HOST, typeEnum } from "./commonApi";
 import { Await } from "react-router-dom";
 
 const CourseOffering = async () => {
@@ -8,4 +8,33 @@ const CourseOffering = async () => {
   return res.data;
 };
 
-export default {CourseOffering};
+const readAll = async () => {
+  console.log("courseOffering이 가동되었다.");
+  const res = await axios.get(`${API_SERVER_HOST}/api/courseoffering/all`);
+  return res.data;
+};
+
+// 세팅
+const config = {
+  tableInfo: {
+    tableEng: "CourseOffering",
+    tableName: "개설강의",
+  },
+  columns: {
+    offeringId: "개설강의코드",
+    professorName: "교수이림",
+    courseName: "강의이름",
+    year: "년도",
+    semester: "학기",
+    course: "강의정보",
+    user: "유저",
+    enrollments: "수강내역들",
+    timeTables: "시간표들",
+  },
+  excludeList: ["course", "user","enrollments","timeTables"],
+  funcs: { CourseOffering, readAll },
+  formData: {},
+  type: typeEnum.default
+};
+
+export default { config, CourseOffering };
