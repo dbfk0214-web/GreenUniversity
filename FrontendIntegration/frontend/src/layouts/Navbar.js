f3f774d18e7ea74609c83f1585cfab552dc2aa53
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-17 01:33:52 +0900

    대학교 Navbar버그 수정
    기존에 마감을 덜해서 에러가 뜨던 코드 수정하였습니다.

--- FrontendIntegration/frontend/src/layouts/Navbar.js
+++ FrontendIntegration/frontend/src/layouts/Navbar.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
 const Navbar = ({children}) => {
-  const drawMap = (mapData,keys) => {
-    return ({mapData && mapData.map(data => 
-        (
-          <h1>{data}</h1>
-        )
-      )});
-  }
 
-  const {arrayData} = children;
-  console.log(arrayData);
+
+
+  // const {arrayData} = children;
+  // console.log(arrayData);
 
   return (
     <div>
-      {map}
+      {/* {drawMap(mapData)} */}
     </div>
   )
 }
