5265aea67000cd4fa9fb084f4edfc96585956cec
JeonByeol <star1004szx@naver.com>
2025-12-12 18:06:47 +0900

    백앤드에서 받아오는 DTO json형식으로 변환하는
    Definitions파일 데이터 짜잘한거 수정했습니다

--- FrontendIntegration/frontend/src/api/definitions/ClassSectionDefinition.js
+++ FrontendIntegration/frontend/src/api/definitions/ClassSectionDefinition.js
@@ -1,5 +1,5 @@
 export const ClassSectionDef = {
-  key: "classsection",
+  key: "section",
   primaryKey: "sectionId",
   tableEng: "ClassSection",
   tableName: "분반내역",
@@ -19,6 +19,9 @@ export const ClassSectionDef = {
       sectionId: "분반 내역 아이디",
       sectionName: "분반명",
       maxCapacity: "정원",
+      availableSeats: "남은정원",
+      isFull: "마감여부",
+      enrollmentRate: "수강률",
       currentCount: "현재 수강 신청 인원수",
       offeringId: "개설강의 id",
       courseName: "강의명",
