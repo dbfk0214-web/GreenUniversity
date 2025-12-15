c1230704d0a4eeefe51cbb4294f2347195369f34
Joheoungwoo <hataraci9350@daum.net>
2025-12-15 16:41:06 +0900

    현재까지 작업본입니ㅏㄷ.

--- FrontendIntegration/frontend/src/components/aboutGreen/GreenVisionComponent.js
+++ FrontendIntegration/frontend/src/components/aboutGreen/GreenVisionComponent.js
@@ -1,35 +1,50 @@
-import React, { useEffect, useState } from "react";
-// import GreenSpirit from "../../json/aboutGreen/green_spirit.json";
-// import { makeSection } from "../../util/makeDivUtils/aboutGreen/makeGreenVision";
+import React from "react";
+import GreenSpirit from "../../json/aboutGreen/green_spirit.json";
+import {
+  makeCommonHeading,
+  makeCommonTitle,
+} from "../../util/makeDivUtils/makeCommonText";
 
 const GreenVisionComponent = () => {
-  const columns = ["title", "description"];
-
   return (
-    <div>
+    <div className="space-y-8">
       {/* title */}
-      <div>
-        <h1>건학정신,연세이념</h1>
+      <div>{makeCommonTitle("건학정신, 연세이념")}</div>
+
+      {/* section */}
+      <div className="border-t border-gray-300">
+        {GreenSpirit &&
+          GreenSpirit.sections.map((section, idx) => (
+            <div key={idx} className="flex border-b border-gray-300">
+              {/* 좌측 타이틀 */}
+              <div className="w-48 shrink-0 border-r border-gray-300 px-6 py-6">
+                {makeCommonHeading(section.title, {
+                  level: 2,
+                  className: "text-blue-700 font-semibold",
+                })}
+              </div>
+
+              {/* 우측 내용 */}
+              <div className="flex-1 px-6 py-6 leading-relaxed text-gray-800">
+                {section.description}
+              </div>
+            </div>
+          ))}
       </div>
 
-
-      {/* 이미지  */}
-      <div>
-        <img src="test" alt="대체 이미지" />
+      {/* 이미지 */}
+      <div className="pt-6 w-full max-w-md">
+        <img
+          src="https://www.yonsei.ac.kr/sites/sc/images/sub/truth-icon.png"
+          alt="진리 아이콘"
+        />
+        <img
+          src="https://www.yonsei.ac.kr/sites/sc/images/sub/freedom-icon.png"
+          alt="자유 아이콘"
+        />
       </div>
     </div>
   );
 };
 
 export default GreenVisionComponent;
-
-
-      // {/* section */}
-      // <div>
-      //   {GreenSpirit &&
-      //     GreenSpirit.sections.map((section) => (
-      //       <div style={{ border: "1px solid #ccc", display: "flex" }}>
-      //         {makeSection(section.title, section.description)}
-      //       </div>
-      //     ))}
-      // </div>
--- FrontendIntegration/frontend/src/components/aboutGreen/HistoryOfGreenComponent.js
+++ FrontendIntegration/frontend/src/components/aboutGreen/HistoryOfGreenComponent.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import GreenHistory from "../../json/aboutGreen/green_history.json";
-// import {
-//   makeMainSection,
-//   makeTimeLine,
-// } from "../../util/makeDivUtils/aboutGreen/makeGreenHistory";
+import {
+  makeMainSection,
+  makeTimeLine,
+} from "../../util/makeDivUtils/aboutGreen/makeGreenHistory";
+import { makeCommonLabel } from "../../util/makeDivUtils/makeCommonMedia";
+import { makeCommonCard } from "../../util/makeDivUtils/makeCommonCard";
+import {
+  makeCommonHeading,
+  makeCommonTitle,
+} from "../../util/makeDivUtils/makeCommonText";
 
 const HistoryOfGreenComponent = () => {
   const [mainSection, setMainSection] = useState(GreenHistory.mainSection);
@@ -13,30 +19,46 @@ const HistoryOfGreenComponent = () => {
   return (
     <div>
       {/* 타이틀 */}
-      <div>{GreenHistory.pageTitle}</div>
-
+      <div>{makeCommonTitle(GreenHistory.pageTitle)}</div>
       {/* MainSection */}
-    
+      <div className="relative rounded-2xl overflow-hidden bg-[#0b2a3d]">
+        <img
+          src={mainSection.imageUrl}
+          alt=""
+          className="w-full h-[260px] object-contain"
+        />
+
+        {/* 텍스트 */}
+        <div className="absolute top-0 left-0 p-8 text-white">
+          {makeCommonHeading(mainSection.title[0], 2, "bg-blue-600 px-3 py-1")}
+          {makeCommonHeading(mainSection.title[1], 3, "text-yellow-300 mt-2")}
+        </div>
+      </div>
+      {/* 설명 */}
+      <div>{makeCommonLabel(mainSection.description)}</div>
+
+      {/* 도표 */}
+      <div></div>
     </div>
   );
 };
 
 export default HistoryOfGreenComponent;
 
-  // <div>
-  //       {mainSection &&
-  //         makeMainSection(
-  //           mainSection.title,
-  //           mainSection.description,
-  //           mainSection.imageUrl
-  //         )}
-  //     </div>
+// <div>
+//       {mainSection &&
+//         makeMainSection(
+//           mainSection.title,
+//           mainSection.description,
+//           mainSection.imageUrl
+//         )}
+//     </div>
 
-  //     {/* 도표 */}
-  //     <div>
-  //       {mainSection &&
-  //         makeTimeLine(
-  //           GreenHistory.timeline.leftColumn,
-  //           GreenHistory.timeline.rightColumn
-  //         )}
-  //     </div>
+//     {/* 도표 */}
+//     <div>
+//       {mainSection &&
+//         makeTimeLine(
+//           GreenHistory.timeline.leftColumn,
+//           GreenHistory.timeline.rightColumn
+//         )}
+//     </div>
