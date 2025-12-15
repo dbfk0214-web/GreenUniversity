75a19407304a1077de24369a914cdf5a552e73ea
Joheoungwoo <hataraci9350@daum.net>
2025-12-15 17:47:52 +0900

    그린소개완료

--- FrontendIntegration/frontend/src/components/aboutGreen/HistoryOfGreenComponent.js
+++ FrontendIntegration/frontend/src/components/aboutGreen/HistoryOfGreenComponent.js
@@ -1,64 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import GreenHistory from "../../json/aboutGreen/green_history.json";
 import {
   makeMainSection,
   makeTimeLine,
-} from "../../util/makeDivUtils/aboutGreen/makeGreenHistory";
-import { makeCommonLabel } from "../../util/makeDivUtils/makeCommonMedia";
-import { makeCommonCard } from "../../util/makeDivUtils/makeCommonCard";
-import {
-  makeCommonHeading,
-  makeCommonTitle,
-} from "../../util/makeDivUtils/makeCommonText";
+} from "../../util/makeDivUtils/makeForced";
+import { makeCommonTitle } from "../../util/makeDivUtils/makeCommonText";
 
 const HistoryOfGreenComponent = () => {
-  const [mainSection, setMainSection] = useState(GreenHistory.mainSection);
-
-  const columns = ["date", "description", "imageUrl"];
+  const [mainSection] = useState(GreenHistory.mainSection);
 
   return (
-    <div>
-      {/* 타이틀 */}
+    <div className="space-y-16">
+      {/* page title */}
       <div>{makeCommonTitle(GreenHistory.pageTitle)}</div>
-      {/* MainSection */}
-      <div className="relative rounded-2xl overflow-hidden bg-[#0b2a3d]">
-        <img
-          src={mainSection.imageUrl}
-          alt=""
-          className="w-full h-[260px] object-contain"
-        />
 
-        {/* 텍스트 */}
-        <div className="absolute top-0 left-0 p-8 text-white">
-          {makeCommonHeading(mainSection.title[0], 2, "bg-blue-600 px-3 py-1")}
-          {makeCommonHeading(mainSection.title[1], 3, "text-yellow-300 mt-2")}
-        </div>
-      </div>
-      {/* 설명 */}
-      <div>{makeCommonLabel(mainSection.description)}</div>
+      {/* main section */}
+      {mainSection &&
+        makeMainSection(
+          mainSection.title,
+          mainSection.description,
+          mainSection.imageUrl
+        )}
 
-      {/* 도표 */}
-      <div></div>
+      {/* timeline */}
+      {makeTimeLine(
+        GreenHistory.timeline.leftColumn,
+        GreenHistory.timeline.rightColumn
+      )}
     </div>
   );
 };
 
 export default HistoryOfGreenComponent;
-
-// <div>
-//       {mainSection &&
-//         makeMainSection(
-//           mainSection.title,
-//           mainSection.description,
-//           mainSection.imageUrl
-//         )}
-//     </div>
-
-//     {/* 도표 */}
-//     <div>
-//       {mainSection &&
-//         makeTimeLine(
-//           GreenHistory.timeline.leftColumn,
-//           GreenHistory.timeline.rightColumn
-//         )}
-//     </div>
